f08cfc35a5cd5c6d260dbf765ac320ea076281f5
Fredrik Axelsson <vainviper@hotmail.com>
2018-09-24 19:05:04 +0200

    add year to collection list

--- public/scripts/confirm.js
+++ public/scripts/confirm.js
@@ -2,9 +2,9 @@ const showButton = document.querySelector('.showButton');
 const showedButton = document.querySelector('.confirmForm');
 showButton.addEventListener('click', function() {
     showedButton.classList.toggle('hidden')
-    test();
+    compareInput();
 });
-function test () {
+function compareInput () {
     const form = document.querySelector("#form");
     form.addEventListener("input", () => {
         if(form.confirmText.value.toLowerCase() === form.equals.value.toLowerCase()) {
